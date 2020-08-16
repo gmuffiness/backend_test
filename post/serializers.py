06df2773719798d06fcdf9e6db55a9af7eafc1be
gmuffiness <35298963+gmuffiness@users.noreply.github.com>
2020-08-16 23:57:30 +0900

    followed

--- post/serializers.py
+++ post/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Post, Content
 
 class PostSerializer(serializers.ModelSerializer):
+    thumbnail = serializers.ImageField(use_url=True)
+
     class Meta:
         model = Post
         fields = ('title','body','pub_date','target_amount','thumbnail')
